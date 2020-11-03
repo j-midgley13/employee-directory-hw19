ce1b850ad3ead77970721ca8cd54d3caf542e652
Jameson Midgley <jameson.midgley@gmail.com>
2020-11-02 20:06:48 -0700

    created basic header

---
+++ employee-dir/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function Header(){
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <h6>Navigate through list of employees here!</h6>
+        </div>
+    )
+};
+
+export default Header
